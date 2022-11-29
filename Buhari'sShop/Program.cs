82bffe6a8cf42c12f65cc00621b33be5cc901cf2
Rachel Okorie <jayrachel96@gmail.com>
2022-11-29 01:42:48 +0100

    Removed comments that were not necessary

--- Buhari'sShop/Program.cs
+++ Buhari'sShop/Program.cs
@@ -1,14 +1,5 @@
 ﻿namespace Buhari_sShop
 {
-
-    //price of items
-    //customer name
-    //payment plans
-    //duration of payments
-    //paymentcompleted?
-    //remaining amt
-    //first deposit
-    //amt left to installment
     internal class Program
     {
         static void Main(string[] args)
